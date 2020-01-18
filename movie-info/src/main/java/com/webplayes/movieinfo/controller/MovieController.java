ae19e2a1c5c9c6243e398d40612b66dc348b7ebf
Gil Ben Shimon <gilb77@gmail.com>
2020-01-18 23:50:33 +0200

    fix the test

--- movie-info/src/main/java/com/webplayes/movieinfo/controller/MovieController.java
+++ movie-info/src/main/java/com/webplayes/movieinfo/controller/MovieController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -22,8 +19,8 @@ public class MovieController {
     private MovieServiceImpl movieService;
 
 
-    @GetMapping(produces = {"application/json"}, path = "movie")
-    public ResponseEntity<Movie> getMovieByName(@RequestParam(value = "name", required = false) String name) {
+    @GetMapping("movie/{name}")
+    public ResponseEntity<Movie> getMovieByName(@PathVariable("name") String name) {
         Movie movie = Movie.builder().uuid(UUID.randomUUID()).name(name).build();
         return new ResponseEntity<>(movie, HttpStatus.OK);
     }
